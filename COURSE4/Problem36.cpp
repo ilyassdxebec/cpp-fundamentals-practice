78d6af1bd424c307a091f0a7a223b0ff86bd32a2
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-01-19 09:28:42 +0800

    Going strong

--- COURSE4/Problem36.cpp
+++ COURSE4/Problem36.cpp
@@ -1,42 +1,40 @@
 #include<iostream>
 using namespace std;
-    enum enOpType{Add='+',Subtract='-',Multiply='*',Devide='/'};
-
-    float TakeNumber(string message){
-        float number;
-        cout<<message;
-        cin>>number;
-        return number;
+enum enOpType{Add = '+',Devide = '/',Subtract = '-',Multiply = '*'};
+float TakeNumber(string message){
+    float number;
+    cout<<message;
+    cin>>number;
+    return number;
+}
+enOpType TakeOperationType(){
+    char OpType;
+    cout<<"\n Please enter operation type: ";
+    cin>>OpType;
+    return (enOpType)OpType;
+}
+float CalculateOperationOfTwoNumbers(float number1,float number2,enOpType OT){
+    switch (OT)
+    { 
+       case Add:
+       return number1+number2;
+       case Subtract:
+       return number1-number2;
+       case Multiply:
+       return number1*number2;
+       case Devide:
+       if(number2==0){
+        cout<<"\ncan't devide by 0";
+        return 0;
+       }else return number1/number2;
+       default:
+       cout<<"\n operation type not recognized!";
+       return 0;
     }
-    enOpType ReadOperationType(){
-        char OpType;
-        cout<<"\nPlease enter operation type: ";
-        cin>>OpType;
-        return (enOpType)OpType;
-    }
-    float OperationOnTwoNumbers(float number1,float number2,enOpType OT){
-        switch (OT)
-        {
-           case Add:
-           return number1+number2;
-           case Subtract:
-           return number1-number2;
-           case Multiply:
-           return number1*number2;
-           case Devide:
-             if(number2==0){
-                cout<<"\nCan't Devide By 0!";
-                return 0;
-             }else return number1/number2;
-             default:
-               return number1+number2;
-        }
-    }
-
+}
 int main(){
-   float Number1 = TakeNumber("Please Enter First Number: ");
-   cout<<endl;
-   float Number2 = TakeNumber("Please Enter Second Number: ");
-   enOpType OpType = ReadOperationType();
-   cout<<"\n "<<Number1<<" "<<(char)OpType<<" "<<Number2<<" is "<<OperationOnTwoNumbers(Number1,Number2,OpType);
+  float number1 = TakeNumber("Please enter the first number: ");
+  float number2 = TakeNumber("\nPlease enter the second number: ");
+  enOpType OT = TakeOperationType();
+  cout<<number1<<" "<<(char)OT<<" "<<number2<<" is "<<CalculateOperationOfTwoNumbers(number1,number2,OT);
 }
