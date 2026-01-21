0dd78b52e7cfadc4edf8d0198051288e0322e940
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-01-22 00:36:17 +0800

    Going strong

---
+++ COURSE4/Problem46.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void PrintLettersFromAtoZ(){
+    for(int i=65;i<=90;i++){
+         cout<<endl<<(char)i;
+    }
+}
+int main(){
+  PrintLettersFromAtoZ();
+}
---
+++ COURSE4/Problem47.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+float ReadLoanAmount(string message){
+    float LoanAmount;
+    do{
+        cout << endl << message;
+        cin >> LoanAmount;
+    } while(LoanAmount <= 0);
+    return LoanAmount;
+}
+float ReadMonthlyPayment(string message){
+    float MonthlyPayment;
+    do{
+        cout << endl << message;
+        cin >> MonthlyPayment;
+    } while(MonthlyPayment <= 0);
+    return MonthlyPayment;
+}
+int CalculateNumberOfMonths(float LoanAmount,float MonthlyPayment){
+    return LoanAmount/MonthlyPayment;
+}
+int main(){
+ float LoanAmount = ReadLoanAmount("Please enter loan amount: ");
+ float MonthlyPayment = ReadMonthlyPayment("Please enter your monthly payment: ");
+ cout<<"You need to be paying for "<<CalculateNumberOfMonths(LoanAmount,MonthlyPayment)<<" Months";
+}
