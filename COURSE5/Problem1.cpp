2d4a8fc694d41533b72133708c2cc80ece477aaa
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-01-23 20:18:27 +0800

    Going strong

--- COURSE5/Problem1.cpp
+++ COURSE5/Problem1.cpp
@@ -1,29 +1,27 @@
 #include<iostream>
 using namespace std;
-
-    void PrintTableHeader(){
-        cout<<"\t\t\tMultiplication Table From 1 to 10\n"<<endl;
-        cout<<"\t";
-        for(int i=1;i<=10;i++){
-         cout<<i<<"\t";
+void PrintTableHeader(){
+    cout<<"\t\t\t***Multiplication Table From 1 to 0***\n\n";
+    cout<<"\t";
+    for(int i=1;i<=10;i++){
+        cout<<i<<"\t";
+    }
+    cout<<"\n__________________________________________________________________________________\n";
+}
+string PrintColumnSeparator(int i){
+    if(i<10) return "  |";
+    else return " |";
+}
+void PrintTable(){
+    PrintTableHeader();
+    for(int i=1;i<=10;i++){
+        cout<<i<<PrintColumnSeparator(i);
+        for(int j=1;j<=10;j++){
+          cout<<"\t"<<j*i;
         }
-        cout<<"\n___________________________________________________________________________________\n";
-    }
-    string ColumnSeperator(int i){
-       if(i<10) return "  |";
-       else return " |";
-    }
-    
-    void PrintTable(){
-        PrintTableHeader();
-        for(int i=1;i<=10;i++){
-            cout<<i<<ColumnSeperator(i);
-            for(int j=1;j<=10;j++){
-             cout<<"\t"<<i*j;
-            }
-            cout<<"\n";
-        }
-    }
+        cout<<"\n";
+    } 
+}
 int main(){
- PrintTable();
+PrintTable();
 }
