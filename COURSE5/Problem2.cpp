5ae9e7d284097efc741c57c52852ddb6b10ddbf3
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-01-24 00:26:21 +0800

    Going strong

--- COURSE5/Problem2.cpp
+++ COURSE5/Problem2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 enum enPrime{Prime,NotPrime};
 
 enPrime CheckNumberType(int num){
+    if(num <= 1) return enPrime::NotPrime; 
    int mid=num/2;
    for(int i=2;i<=mid;i++){
       if(num%i==0){
@@ -12,7 +13,21 @@ enPrime CheckNumberType(int num){
    }
    return enPrime::Prime;
 }
+int ReadPositiveNumber(string message){
+    int number;
+    do{
+        cout << endl << message;
+        cin >> number;
+    } while(number <= 0);
+    return number;
+}
+void PrintPrimeNumbers(){
+ int number = ReadPositiveNumber("Please enter a \"positive\" number and we'll print all prime numbers from 1 to it: ");
+   for(int i=1;i<=number;i++){
+        if(CheckNumberType(i)==enPrime::Prime) cout<<i<<endl;   
+   }
+}
 
 int main(){
-
+   PrintPrimeNumbers();
 }
