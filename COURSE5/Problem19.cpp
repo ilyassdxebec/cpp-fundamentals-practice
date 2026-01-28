5e069ae00991cfc3a340f58dd97446ce4980c688
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-01-29 02:33:08 +0800

    Going strong

--- COURSE5/Problem19.cpp
+++ COURSE5/Problem19.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
- int RandomNumber(int from,int to){
-    int RandomNum = rand()%(to-from+1)+from;
-    return RandomNum;
- }
+int RandomNumber(int from,int to){
+  int RandomNumber = rand()%(to-from+1)+from;
+  return RandomNumber;
+}
 int main(){
-    //this srand is important one time to be declared in the main for the cpp imbedded rand() function to work,don't care how
  srand((unsigned)time(NULL));
 
- cout<<RandomNumber(0,5);
+ cout<<RandomNumber(5,100)<<endl;
+
 }
