4f5d82e37da2f26da2ae5bf9fd9633d8aad741fa
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-01-31 00:02:05 +0800

    Going strong

---
+++ COURSE5/Problem23.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int RandomNumber(int from,int to){
+  int RandomNumber = rand()%(to-from+1)+from;
+  return RandomNumber;
+}
+
+void RandomArrayFilling(int arr[100], int &arrLength){
+   arrLength = ReadPositiveNumber("Enter number of elements: ");
+    for (int i = 0; i < arrLength; i++)
+    {
+        arr[i] = RandomNumber(1,100);
+    }
+    cout << endl;
+}
+
+void PrintArray(int arr[100], int arrLength){
+       cout<<"Array Elements: ";
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int main(){
+ srand((unsigned)time(NULL));
+
+ int arrLength,arr[100];
+ RandomArrayFilling(arr,arrLength);
+ PrintArray(arr,arrLength);
+}
