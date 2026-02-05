06d79de9df9e2ef4f45fdbfa4bf1ff97f88f3194
ILYASS_XEBEC <150360661+ilyasszn492@users.noreply.github.com>
2026-02-06 02:10:17 +0800

    ss

---
+++ COURSE5/Problem40.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int ReadPositiveNumber(string message){
+    int number;
+    do{
+        cout << endl << message;
+        cin >> number;
+    } while(number <= 0);
+    return number;
+}
+void PrintArray(int arr[100], int arrLength){
+     // cout<<"Array 1 Elements: \n"; 
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+void AddElementsToArray(int arr[],int &arrLength,int Number){
+  arrLength++;
+  arr[arrLength-1] = Number;
+}
+
+int main(){
+ srand((unsigned)time(NULL));
+ int arr1[] = {10,10,10,50,50,70,70,70,70,90},arr2[100],arr1Length=10,arr2Length=0;
+
+ cout<<"\nArray 1 Elements: ";
+ PrintArray(arr1,arr1Length);
+ 
+
+}
